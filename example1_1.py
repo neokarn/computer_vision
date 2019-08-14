e6363c89e7a612af3c5e2d90918366a93fb73935
neokarn <34811101+neokarn@users.noreply.github.com>
2019-08-14 14:01:38 +0700

    Create example1_1.py

---
+++ example1_1.py
@@ -0,0 +1,15 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    ret,im = cap.read()
+
+    cv2.imshow('camera',im)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
