b11208c028c0d002366d63b73b188537fa153cbc
neokarn <34811101+neokarn@users.noreply.github.com>
2019-09-06 07:25:16 +0700

    Add files via upload

---
+++ example4_2.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
+count = 0
+charlist = "ABCDF"
+colorlist = ["red","green","blue","olive","cyan"]
+
+hog = cv2.HOGDescriptor((50,50),(50,50),(50,50),(50,50),9)
+#WinSize, BlockSize, BlockStride, CellSize, NBins
+
+label_train = np.zeros((25,1))
+
+for char_id in range(0,5):
+    for im_id in range(1,6):
+        im = cv2.imread("AtoF//"+charlist[char_id]+"//"+str(im_id)+".bmp",0)
+
+        im = cv2.resize(im, (50, 50))
+        im = cv2.GaussianBlur(im, (3, 3), 0)
+        h = hog.compute(im)
+
+        if count == 0:
+            features_train = h.reshape(1,-1)
+        else:
+            features_train = np.concatenate((features_train,h.reshape(1,-1)),axis = 0)
+
+        label_train[count] = char_id
+        count = count+1
+        plt.figure(char_id)
+        plt.plot(h, color=colorlist[char_id])
+        plt.ylim(0,1)
+        plt.figure(5)
+        plt.plot(h,color=colorlist[char_id])
+        plt.ylim(0, 1)
+
+print(features_train)
+print(label_train)
+plt.show()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
