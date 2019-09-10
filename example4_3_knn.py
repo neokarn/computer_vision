861dabbf45cf7feb8896ff9df43ac2f390617ced
neokarn <34811101+neokarn@users.noreply.github.com>
2019-09-10 20:00:27 +0700

    Update example4_3_knn.py

--- example4_3_knn.py
+++ example4_3_knn.py
@@ -6,12 +6,13 @@ charlist = "ABCDF"
 
 hog = cv2.HOGDescriptor((50,50),(50,50),(50,50),(50,50),9)
 #hog = cv2.HOGDescriptor((50,50),(20,20),(10,10),(10,10),9)
+#WinSize, BlockSize, BlockStride, CellSize, NBins
 
 label_train = np.zeros((25,1))
 
 for char_id in range(0,5):
     for im_id in range(1,6):
-        im = cv2.imread("C://Users//surface//Google Drive//Computer Vision//2559-01//MATLAB//AtoF//"+charlist[char_id]+"//"+str(im_id)+".bmp",0)
+        im = cv2.imread("AtoF//"+charlist[char_id]+"//"+str(im_id)+".bmp",0)
 
         im = cv2.resize(im, (50, 50))
         im = cv2.GaussianBlur(im, (3, 3), 0)
@@ -29,7 +30,7 @@ knn = cv2.ml.KNearest_create()
 knn.train(features_train.astype(np.float32),cv2.ml.ROW_SAMPLE,label_train.astype(np.int32))
 
 for im_id in range(1,26):
-    im = cv2.imread("C://Users//surface//Google Drive//Computer Vision//2559-01//MATLAB//AtoF//Unknown//" + str(im_id) + ".bmp", 0)
+    im = cv2.imread("AtoF//Unknown//" + str(im_id) + ".bmp", 0)
 
     im = cv2.resize(im, (50, 50))
     im = cv2.GaussianBlur(im, (3, 3), 0)
