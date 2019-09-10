d55cf0ac437c7ddec35447d9c86769575c28ab52
neokarn <34811101+neokarn@users.noreply.github.com>
2019-09-10 20:05:30 +0700

    Update example4_4_svm.py

--- example4_4_svm.py
+++ example4_4_svm.py
@@ -3,9 +3,10 @@ import cv2
 
 count = 0
 charlist = "ABCDF"
+answerlist = "AAAAABBBBBCCCCCDDDDDFFFFF"
 
-hog = cv2.HOGDescriptor((50,50),(50,50),(50,50),(50,50),9)
-#hog = cv2.HOGDescriptor((50,50),(20,20),(10,10),(10,10),9)
+#hog = cv2.HOGDescriptor((50,50),(50,50),(50,50),(50,50),9)
+hog = cv2.HOGDescriptor((50,50),(20,20),(10,10),(10,10),9)
 #WinSize, BlockSize, BlockStride, CellSize, NBins
 
 label_train = np.zeros((25,1))
@@ -28,6 +29,9 @@ for char_id in range(0,5):
 
 svm = cv2.ml.SVM_create()
 svm.setKernel(cv2.ml.SVM_LINEAR)
+#svm.setType(cv2.ml.SVM_C_SVC)
+#svm.setDegree(20)
+#svm.setGamma(0.15)
 svm.train(features_train.astype(np.float32), cv2.ml.ROW_SAMPLE,label_train.astype(np.int32))
 
 for im_id in range(1,26):
@@ -37,8 +41,12 @@ for im_id in range(1,26):
     im = cv2.GaussianBlur(im, (3, 3), 0)
     h = hog.compute(im)
     result = svm.predict(h.reshape(1,-1).astype(np.float32))[1]
-    cv2.imshow(str(im_id)+"="+charlist[result[0][0].astype(int)],im)
-    cv2.moveWindow(str(im_id)+"="+charlist[result[0][0].astype(int)],100+((im_id-1)%5)*70,np.floor((im_id-1)/5).astype(int)*150)
+    im = cv2.cvtColor(im,cv2.COLOR_GRAY2BGR)
+    if answerlist[im_id-1] != charlist[result[0][0].astype(int)]:
+        im[:,:,2] = 255
+        cv2.putText(im, charlist[result[0][0].astype(int)] , (20, 20), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0))
+    cv2.imshow(str(im_id) + "=" + charlist[result[0][0].astype(int)], cv2.resize(im, (100, 100)))
+    cv2.moveWindow(str(im_id) + "=" + charlist[result[0][0].astype(int)], 100 + ((im_id - 1) % 5) * 120, np.floor((im_id - 1) / 5).astype(int) * 150)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
