30139376d789f63adb84c8b1a09c95711a0edec1
neokarn <34811101+neokarn@users.noreply.github.com>
2019-09-12 15:55:33 +0700

    Update and rename example5_4.py to example5_4_GLCM_classification.py

--- example5_4_GLCM_classification.py
+++ example5_4_GLCM_classification.py
@@ -10,7 +10,7 @@ label_train = np.zeros((15*3,1))
 features_train = np.zeros((15*3,4 * L * 3))
 for class_id in range(0,3):
     for im_id in range(1,16):
-        im = cv2.imread("C://Users//surface//Google Drive//gmkarn@gmail.com//Computer Vision//2559-01//MATLAB//Texture//"+class_list[class_id]+"//"+str(im_id)+".jpg")
+        im = cv2.imread("Texture//"+class_list[class_id]+"//"+str(im_id)+".jpg")
         im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
         im_gray = cv2.resize(im_gray, (50, 50))
         im_gray = (im_gray / 16).astype(np.uint8)
@@ -29,7 +29,7 @@ svm.setKernel(cv2.ml.SVM_LINEAR)
 svm.train(features_train.astype(np.float32), cv2.ml.ROW_SAMPLE,label_train.astype(np.int32))
 
 for im_id in range(1,16):
-    im = cv2.imread("C://Users//surface//Google Drive//gmkarn@gmail.com//Computer Vision//2559-01//MATLAB//Texture//Unknown//" + str(im_id) + ".jpg")
+    im = cv2.imread("Texture//Unknown//" + str(im_id) + ".jpg")
     im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     im_gray = cv2.resize(im_gray, (50, 50))
     im_gray = (im_gray / 16).astype(np.uint8)
