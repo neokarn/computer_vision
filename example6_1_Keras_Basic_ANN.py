e780f2961b9da420f2e74e2c4f4840a29a517494
neokarn <34811101+neokarn@users.noreply.github.com>
2019-09-25 21:25:50 +0700

    Update example6_1_Keras_Basic_ANN.py

--- example6_1_Keras_Basic_ANN.py
+++ example6_1_Keras_Basic_ANN.py
@@ -3,6 +3,16 @@ from keras.layers import Input, Dense
 from keras.utils import plot_model
 import numpy as np
 
+#Create model
+input = Input(shape=(3,))
+hidden = Dense(4, activation='tanh')(input)
+output = Dense(2, activation='softmax')(hidden) #Classification (2 classes)
+model = Model(inputs=input, outputs=output)
+
+model.compile(optimizer='sgd',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
 #for plot_model
 #install Graphviz (https://www.graphviz.org/)
 #set PATH to Graphviz\bin (eg. C:\Program Files (x86)\Graphviz2.38\bin)
@@ -10,17 +20,9 @@ import numpy as np
 #conda install -c anaconda pydot
 #pip install pydot
 
-#Create model
-input = Input(shape=(3,))
-hidden = Dense(4, activation='tanh')(input)
-output = Dense(2, activation='softmax')(hidden) #Classification (2 classes)
-model = Model(inputs=input, outputs=output)
+#plot_model(model, to_file='model1.png')
 
-plot_model(model, to_file='model1.png')
 
-model.compile(optimizer='sgd',
-              loss='binary_crossentropy',
-              metrics=['accuracy'])
 
 #Train model
 x_train = np.asarray([[1,0,1],
