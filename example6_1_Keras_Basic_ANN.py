647f8ebc9e22824f36b6f8d909cbc6f3f1761554
neokarn <34811101+neokarn@users.noreply.github.com>
2019-09-26 21:00:27 +0700

    Update example6_1_Keras_Basic_ANN.py

--- example6_1_Keras_Basic_ANN.py
+++ example6_1_Keras_Basic_ANN.py
@@ -1,12 +1,12 @@
 from keras.models import Model
 from keras.layers import Input, Dense
-from keras.utils import plot_model
+#from keras.utils import plot_model
 import numpy as np
 
 #Create model
 input = Input(shape=(3,))
 hidden = Dense(4, activation='tanh')(input)
-output = Dense(2, activation='softmax')(hidden) #Classification (2 classes)
+output = Dense(1, activation='sigmoid')(hidden) #Binary Classification
 model = Model(inputs=input, outputs=output)
 
 model.compile(optimizer='sgd',
@@ -37,18 +37,10 @@ x_train = np.asarray([[1,0,1],
                       [-1, 0, -2],
                       [4, -2, 7],
                       [-1,-1,4]])
-y_train = np.asarray([[1,0],
-                      [1, 0],
-                      [0, 1],
-                      [1,0],
-                      [0,1],
-                      [1,0],
-                      [0,1],
-                      [0,1],
-                      [1,0],
-                      [0,1]])
 
-model.fit(x_train, y_train, epochs=100, batch_size=10)
+y_train = np.asarray([1,1,0,1,0,1,0,0,1,0])
+
+model.fit(x_train, y_train, epochs=200, batch_size=10)
 
 #Test model
 x_test = np.asarray([[1,5,1],
@@ -56,10 +48,7 @@ x_test = np.asarray([[1,5,1],
                      [-5,0,1],
                      [0,0,0]])
 
-y_test = np.asarray([[1,0],
-                    [1,0],
-                    [0,1],
-                    [0,1]])
+y_test = np.asarray([1,1,0,0])
 
 y_pred = model.predict(x_test)
 
