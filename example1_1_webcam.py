6cac876962ea004f9719e13f6de49c4f8eceebc3
neokarn <34811101+neokarn@users.noreply.github.com>
2020-08-07 11:18:39 +0700

    Update example1_1_webcam.py

--- example1_1_webcam.py
+++ example1_1_webcam.py
@@ -1,11 +1,17 @@
 import cv2
 
+#CAP_SIZE = (1280,720)
+
 cap = cv2.VideoCapture(0)
+#cap.set(cv2.CAP_PROP_FRAME_WIDTH, CAP_SIZE[0])
+#cap.set(cv2.CAP_PROP_FRAME_HEIGHT, CAP_SIZE[1])
 
 while(True):
     ret,im = cap.read()
 
-    cv2.imshow('camera',im)
+    #print(im.shape)
+
+    cv2.imshow('camera',im) #uint8
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
