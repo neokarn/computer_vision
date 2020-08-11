066cd2fc0e14e2b2cefd7fc240285c7f85f06d08
neokarn <34811101+neokarn@users.noreply.github.com>
2020-08-11 10:29:25 +0700

    Update example2_3_medianblur_findcontour.py

--- example2_3_medianblur_findcontour.py
+++ example2_3_medianblur_findcontour.py
@@ -16,20 +16,19 @@ while(cap.isOpened()):
     diffg = cv2.cvtColor(diffc,cv2.COLOR_BGR2GRAY)
     bwmask = cv2.inRange(diffg,50,255)
 
-    bwmask = cv2.medianBlur(bwmask,5)
+    bwmask_median = cv2.medianBlur(bwmask,5)
 
-    temp = bwmask.copy()
-    #contourmask,contours,hierarchy = cv2.findContours(temp, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE) 
-    contours, hierarchy = cv2.findContours(temp, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE) 
+    contours,hierarchy = cv2.findContours(bwmask_median, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
     cv2.drawContours(im, contours, -1, (0, 255, 0), 1)
 
     cv2.imshow('bwmask', bwmask)
     cv2.moveWindow('bwmask',10,10)
-    #cv2.imshow('contourmask',contourmask)
-    #cv2.moveWindow('contourmask', 400, 10)
+    cv2.imshow('bwmask_median', bwmask_median)
+    cv2.moveWindow('bwmask_median', 400, 10)
     cv2.imshow('im', im)
     cv2.moveWindow('im', 800, 10)
+
 
 cap.release()
 cv2.destroyAllWindows()
