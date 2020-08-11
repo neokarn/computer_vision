ff7c72ebdfd7b27aab6973afc8b958e21d070f95
neokarn <34811101+neokarn@users.noreply.github.com>
2020-08-11 15:27:29 +0700

    Update example2_4_morphology_boundingbox.py

--- example2_4_morphology_boundingbox.py
+++ example2_4_morphology_boundingbox.py
@@ -1,9 +1,9 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture('ExampleBGSubtraction.avi')
+cap = cv2.VideoCapture('C://Users//surface//Downloads//ExampleBGSubtraction.avi')
 
-_,bg = cap.read()
+haveFrame,bg = cap.read()
 
 while(cap.isOpened()):
     haveFrame,im = cap.read()
@@ -15,24 +15,21 @@ while(cap.isOpened()):
     diffg = cv2.cvtColor(diffc,cv2.COLOR_BGR2GRAY)
     bwmask = cv2.inRange(diffg,50,255)
 
-    bwmask = cv2.medianBlur(bwmask,7)
+    bwmask_median = cv2.medianBlur(bwmask,5)
 
     kernel = np.ones((55,25), np.uint8)
-    bwmask = cv2.morphologyEx(bwmask, cv2.MORPH_CLOSE, kernel)
+    bwmask_close = cv2.morphologyEx(bwmask_median, cv2.MORPH_CLOSE, kernel)
 
-    temp = bwmask.copy()
-    contourmask,contours,hierarchy = cv2.findContours(temp, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    contours,hierarchy = cv2.findContours(bwmask_close, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
     for cnt in contours:
         x, y, w, h = cv2.boundingRect(cnt)
         cv2.rectangle(im, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-    #cv2.drawContours(im, contours, -1, (0, 255, 0), 2)
-
-    cv2.imshow('bwmask', bwmask)
-    cv2.moveWindow('bwmask',10,10)
-    cv2.imshow('contourmask',contourmask)
-    cv2.moveWindow('contourmask', 400, 10)
+    cv2.imshow('bwmask_median', bwmask_median)
+    cv2.moveWindow('bwmask_median',10,10)
+    cv2.imshow('bwmask_close',bwmask_close)
+    cv2.moveWindow('bwmask_close', 400, 10)
     cv2.imshow('im', im)
     cv2.moveWindow('im', 800, 10)
 
