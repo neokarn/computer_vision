ab835b117709e9638aa3689298a45feac40c774f
neokarn <34811101+neokarn@users.noreply.github.com>
2020-08-24 20:32:01 +0700

    Update example3_6_probabilistic_classification.py

--- example3_6_probabilistic_classification.py
+++ example3_6_probabilistic_classification.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
-from matplotlib import pyplot as plt
+
+# Download files from https://drive.google.com/file/d/1XdZLvORnCnfpyBYflh15I58VQrQdVlUe/view?usp=sharing
 
 h_skin_hist = 0
 h_nonskin_hist = 0
@@ -24,21 +25,21 @@ skin_prob = sum(h_skin_hist)/(sum(h_nonskin_hist)+sum(h_skin_hist))
 nonskin_prob = 1-skin_prob
 
 
-im = cv2.imread("SkinTest4.jpg")
+for im_id in range(1,6):
+    im = cv2.imread("SkinTest"+str(im_id)+".jpg")
 
-im_hsv = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
+    im_hsv = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
 
-mask = np.zeros(im_hsv.shape[:2],np.uint8)
-for i in range(0,mask.shape[0]):
-    for j in range (0,mask.shape[1]):
-        class_skin_prob = h_skin_prob[im_hsv[i][j][0]]*s_skin_prob[im_hsv[i][j][1]]*skin_prob
-        class_nonskin_prob = h_nonskin_prob[im_hsv[i][j][0]] * s_nonskin_prob[im_hsv[i][j][1]]*nonskin_prob
-        if class_skin_prob > class_nonskin_prob:
-            mask[i][j] = 255
+    mask = np.zeros(im_hsv.shape[:2],np.uint8)
+    for i in range(0,mask.shape[0]):
+        for j in range (0,mask.shape[1]):
+            class_skin_prob = h_skin_prob[im_hsv[i][j][0]]*s_skin_prob[im_hsv[i][j][1]]*skin_prob
+            class_nonskin_prob = h_nonskin_prob[im_hsv[i][j][0]] * s_nonskin_prob[im_hsv[i][j][1]]*nonskin_prob
+            if class_skin_prob > class_nonskin_prob:
+                mask[i][j] = 255
 
-cv2.imshow("image",im)
-cv2.imshow("skindetection",mask)
+    cv2.imshow("image"+str(im_id),im)
+    cv2.imshow("skindetection"+str(im_id),mask)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
