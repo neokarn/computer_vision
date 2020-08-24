d3571c9efd92aa9a8b94c7b942df1e81bb3a2501
neokarn <34811101+neokarn@users.noreply.github.com>
2020-08-24 20:12:42 +0700

    Update example3_4_svm_predict.py

--- example3_4_svm_predict.py
+++ example3_4_svm_predict.py
@@ -1,6 +1,8 @@
 import numpy as np
 import cv2
 
+# Download files from https://drive.google.com/file/d/1XdZLvORnCnfpyBYflh15I58VQrQdVlUe/view?usp=sharing
+
 for im_id in range(1,4):
     print(im_id)
     im = cv2.imread("SkinTrain"+str(im_id)+".jpg")
@@ -21,37 +23,45 @@ for im_id in range(1,4):
         h_nonskin_all = h_nonskin
         s_nonskin_all = s_nonskin
     else:
-        h_skin_all = np.concatenate((h_skin_all, h_skin))
-        s_skin_all = np.concatenate((s_skin_all, s_skin))
-        h_nonskin_all = np.concatenate((h_nonskin_all, h_nonskin))
-        s_nonskin_all = np.concatenate((s_nonskin_all, s_nonskin))
+        h_skin_all = np.concatenate((h_skin_all,h_skin))
+        s_skin_all = np.concatenate((s_skin_all,s_skin))
+        h_nonskin_all = np.concatenate((h_nonskin_all,h_nonskin))
+        s_nonskin_all = np.concatenate((s_nonskin_all,s_nonskin))
 
-labels = np.zeros((len(h_skin_all) + len(h_nonskin_all), 1))
+labels = np.zeros((len(h_skin_all)+len(h_nonskin_all),1))
 labels[0:len(h_skin_all)] = 1
 
-features = np.zeros((len(h_skin_all) + len(h_nonskin_all), 2))
-features[:, 0] = np.concatenate((h_skin_all, h_nonskin_all))
-features[:, 1] = np.concatenate((s_skin_all, s_nonskin_all))
+print("label's shape: ", labels.shape)
 
+features = np.zeros((len(h_skin_all)+len(h_nonskin_all),2))
+features[:,0] = np.concatenate((h_skin_all,h_nonskin_all))
+features[:,1] = np.concatenate((s_skin_all,s_nonskin_all))
+
+print("features's shape: ", features.shape)
 
 svm = cv2.ml.SVM_create()
 svm.setKernel(cv2.ml.SVM_LINEAR)
+#svm.setKernel(cv2.ml.SVM_POLY)
 #svm.setDegree(2)
 svm.train(features.astype(np.float32), cv2.ml.ROW_SAMPLE, labels.astype(np.int32))
 
-im = cv2.imread("SkinTest1.jpg")
 
-im_hsv = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
-h = im_hsv[:, :, 0]
-s = im_hsv[:, :, 1]
+for im_id in range(1,6):
+    im = cv2.imread("SkinTest"+str(im_id)+".jpg")
 
-features = np.zeros((len(h.reshape(1,-1)[0]),2))
-features[:,0] = h.reshape(1,-1)[0]
-features[:,1] = s.reshape(1,-1)[0]
+    im_hsv = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
+    h = im_hsv[:, :, 0]
+    s = im_hsv[:, :, 1]
 
-responses = svm.predict(features.astype(np.float32))[1]
+    features = np.zeros((len(h.reshape(1,-1)[0]),2))
+    features[:,0] = h.reshape(1,-1)[0]
+    features[:,1] = s.reshape(1,-1)[0]
 
-cv2.imshow("skindetection",responses.reshape(np.shape(h)))
+    print("features's shape (test image): ", features.shape)
+
+    responses = svm.predict(features.astype(np.float32))[1]
+
+    cv2.imshow("skindetection"+str(im_id),responses.reshape(np.shape(h)))
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
