a07ff4aed9c2b53155121f0d8879522eadcfb803
neokarn <34811101+neokarn@users.noreply.github.com>
2020-08-31 19:33:49 +0700

    Update example4_3_knn.py

--- example4_3_knn.py
+++ example4_3_knn.py
@@ -1,6 +1,8 @@
 import numpy as np
 import cv2
 
+#Download files form https://drive.google.com/file/d/1Gii7rvNVkiurytmLwG8HfTRssE2NjVpi/view?usp=sharing
+
 count = 0
 charlist = "ABCDF"
 answerlist = "AAAAABBBBBCCCCCDDDDDFFFFF"
