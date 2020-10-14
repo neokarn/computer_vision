b3d767d35092eac7270bf742ab2e097a5fc560e4
neokarn <34811101+neokarn@users.noreply.github.com>
2020-10-14 19:07:36 +0700

    Update example6_1_Keras_Basic_ANN.py

--- example6_1_Keras_Basic_ANN.py
+++ example6_1_Keras_Basic_ANN.py
@@ -1,6 +1,5 @@
 from keras.models import Model
 from keras.layers import Input, Dense
-#from keras.utils import plot_model
 import numpy as np
 
 #Create model
@@ -11,20 +10,9 @@ model = Model(inputs=input, outputs=output)
 
 model.compile(optimizer='sgd',
               loss='binary_crossentropy',
-              metrics=['accuracy'])
+              metrics=['acc'])
 
 model.summary()
-
-#for plot_model
-#install Graphviz (https://www.graphviz.org/)
-#set PATH to Graphviz\bin (eg. C:\Program Files (x86)\Graphviz2.38\bin)
-#install pydot (https://pypi.org/project/pydot/)
-#conda install -c anaconda pydot
-#pip install pydot
-
-#plot_model(model, to_file='model1.png')
-
-
 
 #Train model
 x_train = np.asarray([[1,0,1],
@@ -40,7 +28,7 @@ x_train = np.asarray([[1,0,1],
 
 y_train = np.asarray([1,1,0,1,0,1,0,0,1,0])
 
-model.fit(x_train, y_train, epochs=200, batch_size=10)
+model.fit(x_train, y_train, epochs=100, batch_size=10)
 
 #Test model
 x_test = np.asarray([[1,5,1],
@@ -52,8 +40,11 @@ y_test = np.asarray([1,1,0,0])
 
 y_pred = model.predict(x_test)
 
+print("y_pred")
 print(y_pred)
 
 score = model.evaluate(x_test, y_test)
 
+print("score")
 print(score)
+
