e7ef8ac0aa8515639c743905609ee581b8e6cee2
neokarn <34811101+neokarn@users.noreply.github.com>
2020-10-17 18:30:21 +0700

    Update example7_1_CNN.py

--- example7_1_CNN.py
+++ example7_1_CNN.py
@@ -7,23 +7,23 @@ import cv2
 
 #Create model
 input = Input(shape = (50,50,1))
-conv1 = Conv2D(8,3,activation='relu')(input)
+conv1 = Conv2D(10,3,activation='relu')(input)
 pool1 = MaxPool2D(pool_size=(2, 2))(conv1)
-conv2 = Conv2D(8,3,activation='relu')(pool1)
+conv2 = Conv2D(20,3,activation='relu')(pool1)
 pool2 = MaxPool2D(pool_size=(2, 2))(conv2)
 flat = Flatten()(pool2)
-hidden = Dense(16, activation='relu')(flat)
+hidden = Dense(12, activation='relu')(flat)
 output = Dense(3, activation='softmax')(hidden)
 model = Model(inputs=input, outputs=output)
 
 model.compile(optimizer='adam',
               loss='categorical_crossentropy',
-              metrics=['accuracy'])
+              metrics=['acc'])
 
 model.summary()
 
 
-#Read data from file
+#Read data from file (download form https://drive.google.com/file/d/1UACFvQ8QCFUQaBWz9umQxbUWLSQlh1co/view?usp=sharing)
 N = 30
 x_train = np.zeros((N,50,50,1),'float')
 y_train = np.zeros((N),'float')
@@ -44,7 +44,6 @@ h = model.fit(x_train, y_train, epochs=20)
 plt.plot(h.history['acc'])
 plt.show()
 
-
 #Test Model
 N = 15
 x_test = np.zeros((N,50,50,1),'float')
@@ -61,10 +60,10 @@ for class_id in range(1,4):
 y_test = to_categorical(y_test)
 
 score = model.evaluate(x_test, y_test)
-print(score)
+print('score (cross_entropy, accuracy):\n',score)
 
 y_pred = model.predict(x_test)
-print(y_pred)
-print(np.argmax(y_pred,axis = -1)+1)
+print('confidence:\n', y_pred)
+print('predicted class:\n', np.argmax(y_pred,axis = -1)+1)
 
 
