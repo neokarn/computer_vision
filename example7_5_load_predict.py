93535d76e1f1ac7f907bb9f71c0f617c8e4bceaa
neokarn <34811101+neokarn@users.noreply.github.com>
2020-10-18 09:28:38 +0700

    Update and rename example7_5_predict_generator.py to example7_5_load_predict.py

--- example7_5_load_predict.py
+++ example7_5_load_predict.py
@@ -1,6 +1,8 @@
 from keras.models import Model, load_model
 from keras.preprocessing.image import ImageDataGenerator
 import pandas as pd
+import numpy as np
+import cv2
 
 
 BATCH_SIZE = 5
@@ -35,3 +37,13 @@ predict = model.predict_generator(
     workers = 1,
     use_multiprocessing=False)
 print('prediction:\n',predict)
+
+
+imgfile = 'rice/images/001_t.bmp'
+test_im = cv2.imread(imgfile, cv2.IMREAD_COLOR)
+test_im = cv2.cvtColor(test_im, cv2.COLOR_BGR2RGB)
+test_im = cv2.resize(test_im, IMAGE_SIZE)
+test_im = test_im / 255.
+test_im = np.expand_dims(test_im, axis=0)
+w_pred = model.predict(test_im)
+print(imgfile, " = ", w_pred[0][0])
