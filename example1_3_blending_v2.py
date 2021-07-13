624fb539e827022901a57456ebebad72a6071f72
neokarn <34811101+neokarn@users.noreply.github.com>
2021-07-13 12:43:14 +0700

    Add files via upload

---
+++ example1_3_blending_v2.py
@@ -0,0 +1,28 @@
+import numpy as np
+import cv2
+
+TARGET_SIZE = (640,360)
+
+cap = cv2.VideoCapture(0)
+
+L = 10
+
+im_list = []
+while(True):
+    ret,im = cap.read()
+    im_resized = cv2.resize(im, TARGET_SIZE)
+    im_flipped = cv2.flip(im_resized, 1).astype(np.float)
+
+    if len(im_list) == L:
+        im_list = im_list[1:]
+
+
+    im_list.append(im_flipped)
+    im_out = sum(im_list)/len(im_list)
+    cv2.imshow('camera',im_out.astype(np.uint8))
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
