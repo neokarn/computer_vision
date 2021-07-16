a06fe7535d9594219c259ea1b7281c1d21d157f5
neokarn <34811101+neokarn@users.noreply.github.com>
2021-07-16 12:27:08 +0700

    Update example2_3_medianblur_findcontour.py

--- example2_3_medianblur_findcontour.py
+++ example2_3_medianblur_findcontour.py
@@ -22,14 +22,23 @@ while(cap.isOpened()):
     #contourmask,contours,hierarchy = cv2.findContours(bwmask_median, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE) #for opencv 3.2.x 3.4.x
     #contourmask,contours,hierarchy = cv2.findContours(bwmask_median.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE) #for opencv 3.1.x
 
-    cv2.drawContours(im, contours, -1, (0, 255, 0), 1)
+    im_out_contour = im.copy()
+    cv2.drawContours(im_out_contour, contours, -1, (0, 255, 0), 1)
+
+    im_out_boundingbox = im.copy()
+    for cnt in contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+        cv2.rectangle(im_out_boundingbox, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
 
     cv2.imshow('bwmask', bwmask)
     cv2.moveWindow('bwmask',10,10)
     cv2.imshow('bwmask_median', bwmask_median)
     cv2.moveWindow('bwmask_median', 400, 10)
-    cv2.imshow('im', im)
-    cv2.moveWindow('im', 800, 10)
+    cv2.imshow('im_out_contour', im_out_contour)
+    cv2.moveWindow('im_out_contour', 10, 350)
+    cv2.imshow('im_out_boundingbox', im_out_boundingbox)
+    cv2.moveWindow('im_out_boundingbox', 400, 350)
 
 
 cap.release()
