6e7acc771e7749a3d58ff5e1f5c845e5f59d0179
neokarn <34811101+neokarn@users.noreply.github.com>
2021-07-30 11:24:09 +0700

    Update example3_3_training_svm.py

--- example3_3_training_svm.py
+++ example3_3_training_svm.py
@@ -3,6 +3,7 @@ import cv2
 
 # Download files from https://drive.google.com/file/d/1XdZLvORnCnfpyBYflh15I58VQrQdVlUe/view?usp=sharing
 
+print('----------------- Process training inputs ---------------------')
 for im_id in range(1,4):
     print(im_id)
     im = cv2.imread("SkinTrain"+str(im_id)+".jpg")
@@ -12,10 +13,10 @@ for im_id in range(1,4):
     h = im_hsv[:,:,0]
     s = im_hsv[:,:,1]
 
-    h_skin = [val for (i, val) in enumerate(h.reshape(1, -1)[0]) if mask.reshape(1, -1)[0][i] >= 128]
-    s_skin = [val for (i, val) in enumerate(s.reshape(1, -1)[0]) if mask.reshape(1, -1)[0][i] >= 128]
-    h_nonskin = [val for (i, val) in enumerate(h.reshape(1, -1)[0]) if mask.reshape(1, -1)[0][i] < 128]
-    s_nonskin = [val for (i, val) in enumerate(s.reshape(1, -1)[0]) if mask.reshape(1, -1)[0][i] < 128]
+    h_skin = h[mask >= 128]
+    s_skin = s[mask >= 128]
+    h_nonskin = h[mask < 128]
+    s_nonskin = s[mask < 128]
 
     if im_id == 1:
         h_skin_all = h_skin
@@ -31,6 +32,8 @@ for im_id in range(1,4):
 labels = np.zeros((len(h_skin_all)+len(h_nonskin_all),1))
 labels[0:len(h_skin_all)] = 1
 
+
+print('------------------------ Training ----------------------------')
 print("label's shape: ", labels.shape)
 
 features = np.zeros((len(h_skin_all)+len(h_nonskin_all),2))
@@ -45,10 +48,17 @@ svm.setKernel(cv2.ml.SVM_LINEAR)
 #svm.setDegree(2)
 svm.train(features.astype(np.float32), cv2.ml.ROW_SAMPLE, labels.astype(np.int32))
 
-responses = svm.predict(features.astype(np.float32))[1]
+print('-----------------Classify any (h,s) value ---------------------')
+test_pixels = np.array([[10,100],
+                        [100,150]])
+responses = svm.predict(test_pixels.astype(np.float32))
 
 print(responses)
+#print(responses[1])
 
+
+print('-----------------Classify all training pixels---------------------')
+responses = svm.predict(features.astype(np.float32))[1]
 acc = np.count_nonzero(responses == labels)*100.0/len(labels)
 
-print(acc)
+print('Accuracy :',acc)
