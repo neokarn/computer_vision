0d86bf659620f7ceedc70de89113979a3710d4d0
neokarn <34811101+neokarn@users.noreply.github.com>
2021-07-30 13:32:59 +0700

    Update example3_4_svm_predict.py

--- example3_4_svm_predict.py
+++ example3_4_svm_predict.py
@@ -3,19 +3,20 @@ import cv2
 
 # Download files from https://drive.google.com/file/d/1XdZLvORnCnfpyBYflh15I58VQrQdVlUe/view?usp=sharing
 
+print('----------------- Process training inputs ---------------------')
 for im_id in range(1,4):
     print(im_id)
-    im = cv2.imread("SkinTrain"+str(im_id)+".jpg")
-    mask = cv2.imread("SkinTrain"+str(im_id)+"_mask.jpg",0)
+    im = cv2.imread("SkinDetection\SkinTrain"+str(im_id)+".jpg")
+    mask = cv2.imread("SkinDetection\SkinTrain"+str(im_id)+"_mask.jpg",0)
 
     im_hsv = cv2.cvtColor(im,cv2.COLOR_BGR2HSV)
     h = im_hsv[:,:,0]
     s = im_hsv[:,:,1]
 
-    h_skin = [val for (i, val) in enumerate(h.reshape(1, -1)[0]) if mask.reshape(1, -1)[0][i] >= 128]
-    s_skin = [val for (i, val) in enumerate(s.reshape(1, -1)[0]) if mask.reshape(1, -1)[0][i] >= 128]
-    h_nonskin = [val for (i, val) in enumerate(h.reshape(1, -1)[0]) if mask.reshape(1, -1)[0][i] < 128]
-    s_nonskin = [val for (i, val) in enumerate(s.reshape(1, -1)[0]) if mask.reshape(1, -1)[0][i] < 128]
+    h_skin = h[mask >= 128]
+    s_skin = s[mask >= 128]
+    h_nonskin = h[mask < 128]
+    s_nonskin = s[mask < 128]
 
     if im_id == 1:
         h_skin_all = h_skin
@@ -31,6 +32,7 @@ for im_id in range(1,4):
 labels = np.zeros((len(h_skin_all)+len(h_nonskin_all),1))
 labels[0:len(h_skin_all)] = 1
 
+print('------------------------ Training ----------------------------')
 print("label's shape: ", labels.shape)
 
 features = np.zeros((len(h_skin_all)+len(h_nonskin_all),2))
@@ -42,12 +44,15 @@ print("features's shape: ", features.shape)
 svm = cv2.ml.SVM_create()
 svm.setKernel(cv2.ml.SVM_LINEAR)
 #svm.setKernel(cv2.ml.SVM_POLY)
-#svm.setDegree(2)
+#svm.setDegree(10)
+#svm.setKernel(cv2.ml.SVM_RBF)
+#svm.setGamma(0.01)
 svm.train(features.astype(np.float32), cv2.ml.ROW_SAMPLE, labels.astype(np.int32))
 
 
+print('----------------- Testing ---------------------')
 for im_id in range(1,6):
-    im = cv2.imread("SkinTest"+str(im_id)+".jpg")
+    im = cv2.imread("SkinDetection\SkinTest"+str(im_id)+".jpg")
 
     im_hsv = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
     h = im_hsv[:, :, 0]
@@ -66,4 +71,3 @@ for im_id in range(1,6):
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
