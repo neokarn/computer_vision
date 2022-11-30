3a3dcd29411afb8bc5afda7fae1bcee3046c3316
neokarn <34811101+neokarn@users.noreply.github.com>
2022-11-30 08:48:42 +0700

    Update and rename example1_5_convertcolor.py to example1_4_convertcolor.py

--- example1_4_convertcolor.py
+++ example1_4_convertcolor.py
@@ -1,6 +1,3 @@
-import numpy as np
-import cv2
-
 cap = cv2.VideoCapture(0)
 
 TARGET_SIZE = (640,360)
@@ -13,14 +10,18 @@ while(True):
     im_gray = cv2.cvtColor(im_flipped,cv2.COLOR_BGR2GRAY)
     im_hsv = cv2.cvtColor(im_flipped, cv2.COLOR_BGR2HSV)
     hue = im_hsv[:,:,0]
-    #hue = cv2.applyColorMap((hue*(255/179)).astype('uint8'),cv2.COLORMAP_HSV)
+    hue = cv2.applyColorMap((hue*(255/179)).astype('uint8'),cv2.COLORMAP_HSV)
     sat = im_hsv[:,:,1]
     val = im_hsv[:,:,2]
 
     cv2.imshow('Gray', im_gray)
+    cv2.moveWindow('Gray',0,0)
     cv2.imshow('Hue', hue)
+    cv2.moveWindow('Hue',TARGET_SIZE[0],0)
     cv2.imshow('Sat', sat)
+    cv2.moveWindow('Sat',0,TARGET_SIZE[1])
     cv2.imshow('Val', val)
+    cv2.moveWindow('Val',TARGET_SIZE[0],TARGET_SIZE[1])
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
