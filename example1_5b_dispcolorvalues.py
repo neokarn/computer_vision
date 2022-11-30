f1d28c29dfec2af5004740c4ba8feb2986ade8b6
neokarn <34811101+neokarn@users.noreply.github.com>
2022-11-30 09:25:17 +0700

    Update and rename example1_4b_dispcolorvalues.py to example1_5b_dispcolorvalues.py

--- example1_5b_dispcolorvalues.py
+++ example1_5b_dispcolorvalues.py
@@ -1,6 +1,3 @@
-import numpy as np
-import cv2
-
 TARGET_SIZE = (640,360)
 
 cap = cv2.VideoCapture(0)
@@ -12,15 +9,23 @@ while(True):
 
     mask = cv2.inRange(im_flipped,(0,0,90),(50,50,255))
     cv2.imshow('mask', mask)
+    cv2.moveWindow('mask',TARGET_SIZE[0],0)
 
     #############################################
     h, w = im_flipped.shape[:2]
-    b = im_flipped[int(h/2),int(w/2),0]
-    g = im_flipped[int(h/2),int(w/2),1]
-    r = im_flipped[int(h/2),int(w/2),2]
+    im_cropped = im_flipped[(int(h/2)-8):(int(h/2)+9),
+                           (int(w/2)-8):(int(w/2)+9),
+                           :]
+    cv2.imshow('cropped', cv2.resize(im_cropped, (128,128)))
+    cv2.moveWindow('cropped',0,TARGET_SIZE[1])
+    b = int(np.mean(im_cropped[:,:,0]))
+    g = int(np.mean(im_cropped[:,:,1]))
+    r = int(np.mean(im_cropped[:,:,2]))
 
-    cv2.putText(im_flipped, 'x', (int(w/2), int(h/2)),
-                cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255))
+    cv2.rectangle(im_flipped,
+                  (int(w/2)-8,int(h/2)-8),
+                  (int(w/2)+8,int(h/2)+8),
+                  (255,255,255))
     cv2.putText(im_flipped, str(b), (20, 30),
                 cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0))
     cv2.putText(im_flipped, str(g), (90, 30),
@@ -35,9 +40,11 @@ while(True):
         cv2.putText(im_flipped,'Coke',(50,100),cv2.FONT_HERSHEY_PLAIN,5,(255,255,255))
 
     cv2.imshow('camera', im_flipped)
+    cv2.moveWindow('camera',0,0)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
 cv2.destroyAllWindows()
+
