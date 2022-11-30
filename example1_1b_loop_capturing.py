ff652ea9855f5a28b14dfd951ff08706e21cdd5c
neokarn <34811101+neokarn@users.noreply.github.com>
2022-11-30 08:21:01 +0700

    Update example1_1b_loop_capturing.py

--- example1_1b_loop_capturing.py
+++ example1_1b_loop_capturing.py
@@ -2,16 +2,10 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
-#CAP_SIZE = (1280,720)
-#cap.set(cv2.CAP_PROP_FRAME_WIDTH, CAP_SIZE[0])
-#cap.set(cv2.CAP_PROP_FRAME_HEIGHT, CAP_SIZE[1])
-
 while(True):
     ret,im = cap.read()
 
-    #print(im.shape)
-
-    cv2.imshow('camera',im) #uint8
+    cv2.imshow('camera',im) 
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
