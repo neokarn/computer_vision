0c8f9e5904969e62e30ddbe304ad70724adde971
neokarn <34811101+neokarn@users.noreply.github.com>
2023-02-25 20:55:20 +0700

    Update example6_6_load_model.py

--- example6_6_load_model.py
+++ example6_6_load_model.py
@@ -1,3 +1,6 @@
+#For Google Colab Version
+#https://colab.research.google.com/drive/1LK-1OMH25pNOpYi11HXuBBHWXL1rx_og?usp=share_link
+
 from keras.models import load_model
 from sklearn.metrics import confusion_matrix
 import numpy as np
