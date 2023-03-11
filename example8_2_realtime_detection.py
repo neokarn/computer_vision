50b9081179ed16df8f11005d95c8d6bb77b13bc4
neokarn <34811101+neokarn@users.noreply.github.com>
2023-03-11 14:28:32 +0700

    Update example8_2_realtime_detection.py

--- example8_2_realtime_detection.py
+++ example8_2_realtime_detection.py
@@ -2,26 +2,29 @@ import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
-orb = cv2.ORB_create()
-bf = cv2.BFMatcher(cv2.NORM_HAMMING)
 
-ref = cv2.imread('onepiece73.jpg', cv2.COLOR_BGR2GRAY)
+detector = cv2.SIFT_create()
+matcher = cv2.BFMatcher()
+
+#detector = cv2.ORB_create()
+#matcher = cv2.BFMatcher(cv2.NORM_HAMMING)
+
+ref = cv2.imread('conan1.jpg', cv2.COLOR_BGR2GRAY)
 h,w,_ = ref.shape
 ref = cv2.resize(ref,(int(w*0.7),int(h*0.7)))
 
-
-kp1, des1 = orb.detectAndCompute(ref, None)
+kp1, des1 = detector.detectAndCompute(ref, None)
 
 while(True):
     _,target = cap.read()
 
-    kp2, des2 = orb.detectAndCompute(target, None)
+    kp2, des2 = detector.detectAndCompute(target, None)
 
-    matches = bf.match(des1, des2)
+    matches = matcher.match(des1, des2)
 
     matches = sorted(matches, key=lambda x: x.distance)
 
-    result = cv2.drawMatches(ref, kp1, target, kp2, matches[:20], None, flags=2)
+    result = cv2.drawMatches(ref, kp1, target, kp2, matches[:50], None, flags=2)
 
     cv2.imshow('result', result)
 
