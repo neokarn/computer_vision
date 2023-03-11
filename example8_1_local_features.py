77daca61efb4b613d276db4f2c7b4eaa88fdfac3
neokarn <34811101+neokarn@users.noreply.github.com>
2023-03-11 14:26:53 +0700

    Update example8_1_local_features.py

--- example8_1_local_features.py
+++ example8_1_local_features.py
@@ -1,17 +1,21 @@
 import cv2
 
+#Download images from
 #https://drive.google.com/file/d/1TbVn2K3Kxtntne19vPOQjf5pGs6Jo72H/view?usp=sharing
-
 
 ref = cv2.imread('conan1.jpg',cv2.COLOR_BGR2GRAY)
 target = cv2.imread('conan2.jpg',cv2.COLOR_BGR2GRAY)
 h,w,_ = target.shape
 target = cv2.resize(target,(int(w*0.5),int(h*0.5)))
 
-orb = cv2.ORB_create()
+detector = cv2.SIFT_create()
+matcher = cv2.BFMatcher()
 
-kp1, des1 = orb.detectAndCompute(ref,None)
-kp2, des2 = orb.detectAndCompute(target,None)
+#detector = cv2.ORB_create()
+#matcher = cv2.BFMatcher(cv2.NORM_HAMMING)
+
+kp1, des1 = detector.detectAndCompute(ref,None)
+kp2, des2 = detector.detectAndCompute(target,None)
 
 print(str(len(kp1))+","+str(len(kp2)))
 print(des1.shape)
@@ -21,18 +25,16 @@ print(kp1[0].size)
 print(kp1[0].angle)
 print(des1[0])
 
-bf = cv2.BFMatcher(cv2.NORM_HAMMING)
-
-matches = bf.match(des1,des2)
+matches = matcher.match(des1,des2)
 
 matches = sorted(matches, key = lambda x:x.distance)
+
+result = cv2.drawMatches(ref,kp1,target,kp2,matches[:50],None, flags=2)
 
 keypoint_ref = ref.copy()
 cv2.drawKeypoints(ref,kp1,keypoint_ref,None,flags=4)
 keypoint_target = target.copy()
 cv2.drawKeypoints(target,kp2,keypoint_target,None,flags=4)
-
-result = cv2.drawMatches(ref,kp1,target,kp2,matches[:30],None, flags=2)
 
 cv2.imshow('keypoint1',keypoint_ref)
 cv2.imshow('keypoint2',keypoint_target)
