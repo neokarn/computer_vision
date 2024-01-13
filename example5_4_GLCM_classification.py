b906a98c323bd1a0f17e2b0556bef58439d4432d
neokarn <34811101+neokarn@users.noreply.github.com>
2024-01-13 08:17:01 +0700

    Update example5_4_GLCM_classification.py

--- example5_4_GLCM_classification.py
+++ example5_4_GLCM_classification.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2
-from skimage.feature import greycomatrix, greycoprops
+from skimage.feature import graycomatrix, graycoprops
 
 #download images form https://drive.google.com/file/d/1JfJYr-qJvgt1Jyz-Gop-oRci-TipuQmb/view?usp=sharing
 
@@ -16,12 +16,12 @@ for class_id in range(0,3):
         im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
         im_gray = cv2.resize(im_gray, (50, 50))
         im_gray = (im_gray / 16).astype(np.uint8)
-        glcm = greycomatrix(im_gray, range(1, L + 1), [0, np.pi / 4, np.pi / 2], 16, symmetric=True, normed=True)
+        glcm = graycomatrix(im_gray, range(1, L + 1), [0, np.pi / 4, np.pi / 2], 16, symmetric=True, normed=True)
         glcm_props = np.zeros(4 * L * 3)
-        glcm_props[0:(L * 3)] = greycoprops(glcm, 'ASM').reshape(1, -1)[0]
-        glcm_props[(L * 3):(L * 3 * 2)] = greycoprops(glcm, 'contrast').reshape(1, -1)[0]
-        glcm_props[(L * 3 * 2):(L * 3 * 3)] = greycoprops(glcm, 'homogeneity').reshape(1, -1)[0]
-        glcm_props[(L * 3 * 3):(L * 3 * 4)] = greycoprops(glcm, 'correlation').reshape(1, -1)[0]
+        glcm_props[0:(L * 3)] = graycoprops(glcm, 'ASM').reshape(1, -1)[0]
+        glcm_props[(L * 3):(L * 3 * 2)] = graycoprops(glcm, 'contrast').reshape(1, -1)[0]
+        glcm_props[(L * 3 * 2):(L * 3 * 3)] = graycoprops(glcm, 'homogeneity').reshape(1, -1)[0]
+        glcm_props[(L * 3 * 3):(L * 3 * 4)] = graycoprops(glcm, 'correlation').reshape(1, -1)[0]
         features_train[count] = glcm_props
         label_train[count] = class_id
         count = count+1
@@ -35,12 +35,12 @@ for im_id in range(1,16):
     im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     im_gray = cv2.resize(im_gray, (50, 50))
     im_gray = (im_gray / 16).astype(np.uint8)
-    glcm = greycomatrix(im_gray, range(1, L + 1), [0, np.pi / 4, np.pi / 2], 16, symmetric=True, normed=True)
+    glcm = graycomatrix(im_gray, range(1, L + 1), [0, np.pi / 4, np.pi / 2], 16, symmetric=True, normed=True)
     glcm_props = np.zeros(4 * L * 3)
-    glcm_props[0:(L * 3)] = greycoprops(glcm, 'ASM').reshape(1, -1)[0]
-    glcm_props[(L * 3):(L * 3 * 2)] = greycoprops(glcm, 'contrast').reshape(1, -1)[0]
-    glcm_props[(L * 3 * 2):(L * 3 * 3)] = greycoprops(glcm, 'homogeneity').reshape(1, -1)[0]
-    glcm_props[(L * 3 * 3):(L * 3 * 4)] = greycoprops(glcm, 'correlation').reshape(1, -1)[0]
+    glcm_props[0:(L * 3)] = graycoprops(glcm, 'ASM').reshape(1, -1)[0]
+    glcm_props[(L * 3):(L * 3 * 2)] = graycoprops(glcm, 'contrast').reshape(1, -1)[0]
+    glcm_props[(L * 3 * 2):(L * 3 * 3)] = graycoprops(glcm, 'homogeneity').reshape(1, -1)[0]
+    glcm_props[(L * 3 * 3):(L * 3 * 4)] = graycoprops(glcm, 'correlation').reshape(1, -1)[0]
     result = svm.predict(glcm_props.reshape(1,-1).astype(np.float32))[1]
     cv2.putText(im, class_list[result[0][0].astype(int)], (10, 30), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0))
     cv2.imshow(str(im_id)+"="+class_list[result[0][0].astype(int)],im)
