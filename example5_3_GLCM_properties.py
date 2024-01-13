2cbae2309c93922a99a7a99e5c1e03f1de422d69
neokarn <34811101+neokarn@users.noreply.github.com>
2024-01-13 08:16:18 +0700

    Update example5_3_GLCM_properties.py

--- example5_3_GLCM_properties.py
+++ example5_3_GLCM_properties.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
-from skimage.feature import greycomatrix, greycoprops
+from skimage.feature import graycomatrix, graycoprops
 
 #download images form https://drive.google.com/file/d/1JfJYr-qJvgt1Jyz-Gop-oRci-TipuQmb/view?usp=sharing
 
@@ -11,15 +11,15 @@ cv2.imshow("image",im)
 
 L = 10
 im_gray = (im_gray/16).astype(np.uint8)
-glcm = greycomatrix(im_gray, range(1,L+1), [0,np.pi/4,np.pi/2], 16, symmetric=True, normed=True)
+glcm = graycomatrix(im_gray, range(1,L+1), [0,np.pi/4,np.pi/2], 16, symmetric=True, normed=True)
 print('GLCM Shape')
 print(glcm.shape)
 
 glcm_props = np.zeros(4*L*3)
-glcm_props[0:(L*3)] = greycoprops(glcm, 'ASM').reshape(1,-1)[0]
-glcm_props[(L*3):(L*3*2)] = greycoprops(glcm, 'contrast').reshape(1,-1)[0]
-glcm_props[(L*3*2):(L*3*3)] = greycoprops(glcm, 'homogeneity').reshape(1,-1)[0]
-glcm_props[(L*3*3):(L*3*4)] = greycoprops(glcm, 'correlation').reshape(1,-1)[0]
+glcm_props[0:(L*3)] = graycoprops(glcm, 'ASM').reshape(1,-1)[0]
+glcm_props[(L*3):(L*3*2)] = graycoprops(glcm, 'contrast').reshape(1,-1)[0]
+glcm_props[(L*3*2):(L*3*3)] = graycoprops(glcm, 'homogeneity').reshape(1,-1)[0]
+glcm_props[(L*3*3):(L*3*4)] = graycoprops(glcm, 'correlation').reshape(1,-1)[0]
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
