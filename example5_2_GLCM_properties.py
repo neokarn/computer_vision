59a9cc81ddb1dbd2897bab24aa5666bd1a5c6c1c
neokarn <34811101+neokarn@users.noreply.github.com>
2024-01-13 08:15:46 +0700

    Update example5_2_GLCM_properties.py

--- example5_2_GLCM_properties.py
+++ example5_2_GLCM_properties.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
-from skimage.feature import greycomatrix, greycoprops
+from skimage.feature import graycomatrix, graycoprops
 
 #download images form https://drive.google.com/file/d/1JfJYr-qJvgt1Jyz-Gop-oRci-TipuQmb/view?usp=sharing
 
@@ -10,21 +10,21 @@ im_gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
 cv2.imshow("image",im)
 
 #glcm = greycomatrix(im_gray, [5], [0], 256, symmetric=True, normed=True)
-glcm = greycomatrix(im_gray, [5,10,15], [0,np.pi/2], 256, symmetric=True, normed=True)
+glcm = graycomatrix(im_gray, [5,10,15], [0,np.pi/2], 256, symmetric=True, normed=True)
 print('GLCM Shape')
 print(glcm.shape)
 
 print('ASM')
-print(greycoprops(glcm, 'ASM'))
+print(graycoprops(glcm, 'ASM'))
 
 print('contrast')
-print(greycoprops(glcm, 'contrast'))
+print(graycoprops(glcm, 'contrast'))
 
 print('homogeneity')
-print(greycoprops(glcm, 'homogeneity'))
+print(graycoprops(glcm, 'homogeneity'))
 
 print('correlation')
-print(greycoprops(glcm, 'correlation'))
+print(graycoprops(glcm, 'correlation'))
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
