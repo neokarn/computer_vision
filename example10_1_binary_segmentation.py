b6c6d6ec63c2d50979b64aea1b7295dadd3fbaf1
neokarn <34811101+neokarn@users.noreply.github.com>
2024-03-03 21:15:48 +0700

    Update example10_1_binary_segmentation.py

--- example10_1_binary_segmentation.py
+++ example10_1_binary_segmentation.py
@@ -4,7 +4,9 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import ModelCheckpoint
 import matplotlib.pyplot as plt
 
+#Colab Version: https://colab.research.google.com/drive/1uhTC-qslMw2J8XBqAIZa9UWvKc9rEYk3?usp=sharing
 #Download dataset from https://drive.google.com/open?id=1wWuxCQJEOQX980LuwSjTBM-EzbOJQtJy
+
 
 BATCH_SIZE = 5
 MAX_EPOCH = 30
